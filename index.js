25bdc17e7ce12ab4fe3a471c03288819d8288b31
Mikael Vesavuori <mikael.vesavuori@humblebee.se>
2018-05-08 13:27:40 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const Firestore = require('@google-cloud/firestore');
+
+const firestore = new Firestore({
+	projectId: { ID },
+	keyFilename: 'keyfile.json'
+});
+
+const collection = firestore.collection('posts');
+
+exports.create = (request, response) => {
+	const body = request.body;
+
+	collection
+		.doc(`${body.title}`)
+		.set({
+			title: body.title,
+			text: body.text
+		})
+		.then(() => {
+			response.sendStatus(200);
+		})
+		.catch(() => {
+			console.warn('Error creating!');
+		});
+};
+
+exports.read = (request, response) => {
+	collection
+		.get()
+		.then(function(querySnapshot) {
+			querySnapshot.forEach(function(doc) {
+				console.log(doc.id, ' => ', doc.data());
+			});
+		})
+		.then(() => {
+			response.sendStatus(200);
+		})
+		.catch(() => {
+			console.warn('Error reading!');
+			response.sendStatus(400);
+		});
+};
+
+exports.update = (request, response) => {
+	const body = request.body;
+
+	collection
+		.doc(`${body.title}`)
+		.set({
+			text: body.text
+		})
+		.then(() => {
+			response.sendStatus(200);
+		})
+		.catch(() => {
+			console.warn('Error updating!');
+			response.sendStatus(400);
+		});
+};
+
+exports.delete = (request, response) => {
+	const body = request.body;
+
+	collection
+		.doc(`${body.title}`)
+		.delete()
+		.then(() => {
+			response.sendStatus(200);
+		})
+		.catch(() => {
+			console.warn('Error deleting!');
+			response.sendStatus(400);
+		});
+};
---
+++ readme.md
@@ -0,0 +1,21 @@
+# CRUD API using Serverless (Google Cloud Functions and Firestore)
+
+This is a basic demonstration for how to do CRUD tasks (Create, Read, Update, Delete) in an API-like way on a Firestore database.
+
+What's so sweet is that you don't need to set up any kind of server or other typical backend stuff – just write your code! Serverless for the win :)
+
+## Get started
+
+1.  Make sure your credentials are in order (read more at [Google](https://serverless.com/framework/docs/providers/google/guide/credentials/))
+2.  Create a `keyfile.json` in the root of the project (read more at [Google](https://cloud.google.com/iam/docs/creating-managing-service-account-keys))
+3.  Update `projectId` in the instance called `firestore`
+4.  Run `serverless deploy`
+
+## CLI Commands
+
+The command `serverless` can also be used in its shorthand form `sls`.
+
+* `serverless invoke -f create -d '{ "title": "Some title", "text": "Content goes here" }`: Create new document in the **posts** collection
+* `serverless invoke -f read`: Read all **posts** documents (outputs to console)
+* `serverless invoke -f update -d '{ "title": "Some title", "text": "This is the new text" }'`: Update a named document with new text
+* `serverless invoke -f delete -d '{ "title": "094BejwD5tF3lQZfKZnR" }'`: Delete a named document
