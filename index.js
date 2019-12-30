ccaeeeb404d2f8c925a2540fc9199e6747ef2c34
Mikael Vesavuori <mikael.vesavuori@humblebee.se>
2019-12-30 11:36:12 +0100

    Bump dependencies; Adjust serverless.yml configs

--- index.js
+++ index.js
@@ -1,77 +1,74 @@
-'use strict';
+"use strict";
 
-const Firestore = require('@google-cloud/firestore');
+const Firestore = require("@google-cloud/firestore");
 
-const firestore = new Firestore({
-	projectId: { ID },
-	keyFilename: 'keyfile.json'
-});
+const firestore = new Firestore();
 
-const collection = firestore.collection('posts');
+const collection = firestore.collection("posts");
 
 exports.create = (request, response) => {
-	const body = request.body;
+  const body = request.body;
 
-	collection
-		.doc(`${body.title}`)
-		.set({
-			title: body.title,
-			text: body.text
-		})
-		.then(() => {
-			response.sendStatus(200);
-		})
-		.catch(() => {
-			console.warn('Error creating!');
-			response.sendStatus(400);
-		});
+  collection
+    .doc(`${body.title}`)
+    .set({
+      title: body.title,
+      text: body.text
+    })
+    .then(() => {
+      response.sendStatus(200);
+    })
+    .catch(() => {
+      console.warn("Error creating!");
+      response.sendStatus(400);
+    });
 };
 
 exports.read = (request, response) => {
-	collection
-		.get()
-		.then(function(querySnapshot) {
-			querySnapshot.forEach(function(doc) {
-				console.log(doc.id, ' => ', doc.data());
-			});
-		})
-		.then(() => {
-			response.sendStatus(200);
-		})
-		.catch(() => {
-			console.warn('Error reading!');
-			response.sendStatus(400);
-		});
+  collection
+    .get()
+    .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        console.log(doc.id, " => ", doc.data());
+      });
+    })
+    .then(() => {
+      response.sendStatus(200);
+    })
+    .catch(() => {
+      console.warn("Error reading!");
+      response.sendStatus(400);
+    });
 };
 
 exports.update = (request, response) => {
-	const body = request.body;
+  const body = request.body;
 
-	collection
-		.doc(`${body.title}`)
-		.set({
-			text: body.text
-		})
-		.then(() => {
-			response.sendStatus(200);
-		})
-		.catch(() => {
-			console.warn('Error updating!');
-			response.sendStatus(400);
-		});
+  collection
+    .doc(`${body.title}`)
+    .set({
+      text: body.text
+    })
+    .then(() => {
+      response.sendStatus(200);
+    })
+    .catch(() => {
+      console.warn("Error updating!");
+      response.sendStatus(400);
+    });
 };
 
 exports.delete = (request, response) => {
-	const body = request.body;
+  const body = request.body;
 
-	collection
-		.doc(`${body.title}`)
-		.delete()
-		.then(() => {
-			response.sendStatus(200);
-		})
-		.catch(() => {
-			console.warn('Error deleting!');
-			response.sendStatus(400);
-		});
+  collection
+    .doc(`${body.title}`)
+    .delete()
+    .then(() => {
+      response.sendStatus(200);
+    })
+    .catch(() => {
+      console.warn("Error deleting!");
+      response.sendStatus(400);
+    });
 };
